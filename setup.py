bdeaf9c7652dadf179ae83838358c84f786e2929
Satoshi Tanimoto <stanimoto@saymedia.com>
2012-05-18 14:42:23 -0700

    fix up

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     name = 'DDLockClient',
     version = version,
     author = 'Satoshi Tanimoto',
+    author_email = 'tanimoto.satoshi@gmail.com',
     description = 'Python client library for the Danga distributed lock daemon',
     long_description = open('README.rst').read(),
     url = 'http://github.com/stanimoto/python-ddlockclient',
